909cd486b7ea403d3ecbf669e990e30412ba9dfd
LesserjL <107967719+LesserjL@users.noreply.github.com>
2023-08-01 08:41:46 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var x = prompt("Do you like capybara");
+var name = prompt("What is your name?");
+if(x === "yes"){
+  alert(name + " likes capybaras!");
+}
+else{
+  alert(name + " does not like capybaras...");
+}
+
